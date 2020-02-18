deb52d9649fa97860f8643909677297f2f1a00b6
xiaofanglei <xiaofanglei@countrygarden.com.cn>
2020-02-18 19:54:05 +0800

    new

---
+++ demo/src/main/java/com/demo/demo/service/Demo1.java
@@ -0,0 +1,82 @@
+package com.demo.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <p> Title: Demo1 </p>
+ * <p> Description: demo1 </p>
+ *
+ * @author xiaofanglei@126.com
+ * @date 2020/02/18
+ **/
+@Service
+@Slf4j
+public class Demo1 {
+    private static String orgPattern = "{i,like,sam,sung,samsung,mobile,ice,cream,man go}";
+
+    private static List<String> splitWords = new ArrayList(Arrays.asList(
+            orgPattern.replace("，", ",").replace("{","")
+                    .replace("}","").split(",")));
+
+    private static List<String> spiltWordsWithoutBlankSpace = new ArrayList(Arrays.asList(orgPattern.replace("，", ",").replace("{","")
+            .replace("}","").replace(" ","").split(",")));
+
+    /**
+     * 直接拆分语句
+     *
+     * @param inputMsg
+     */
+    public void individualDictionaryWords(String inputMsg) {
+        //判断单词是否存在
+        wordExists(inputMsg, "");
+    }
+
+    /**
+     * 根据输入拆分语句
+     *
+     * @param pattern
+     * @param inputMsg
+     */
+    public void individualDictionaryWordsOnlyInput(String pattern, String inputMsg) {
+        this.splitWords = new ArrayList(Arrays.asList(
+                pattern.replace("，", ",").replace("{","")
+                        .replace("}","").split(",")));
+        this.spiltWordsWithoutBlankSpace = new ArrayList(Arrays.asList(pattern.replace("，", ",").replace("{","")
+                .replace("}","").replace(" ","").split(",")));
+
+        //判断单词是否存在
+        wordExists(inputMsg, "");
+    }
+
+    /**
+     * 递归判断之后的单词是否存在
+     *
+     * @param pattern
+     * @param outWords
+     */
+    private void wordExists(String pattern,String outWords) {
+        int leng = pattern.length();
+        //判断单词是否存在
+        for (int index = 1; index <= leng; index++) {
+            String str = pattern.substring(0, index);
+            //如果存在匹配的单词
+            int indexOfWord = spiltWordsWithoutBlankSpace.indexOf(str);
+            if (-1 != indexOfWord) {
+                if (index == leng) {
+                    outWords += splitWords.get(indexOfWord);
+                    //输出一整句话
+                    log.warn(outWords);
+                    return;
+                }
+                //递归判断之后的单词是否存在
+                wordExists(pattern.substring(index, leng), outWords + str + " ");
+            }
+        }
+    }
+
+}
---
+++ demo/src/test/java/com/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,34 @@
+package com.demo.demo;
+
+import com.demo.demo.service.Demo1;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={DemoApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
+public class DemoApplicationTests {
+    @Autowired
+    private Demo1 demo1;
+
+    @Test
+    public void stage1_1() {
+        this.demo1.individualDictionaryWords("ilikesamsungmobile");
+    }
+
+    @Test
+    public void stage1_2() {
+        this.demo1.individualDictionaryWords("ilikeicecreammango");
+    }
+
+    @Test
+    public void stage2(){
+        this.demo1.individualDictionaryWordsOnlyInput("{{i,like,sam,sung,mobile,icecream,man go,mango}",
+                "ilikeicecreammango");
+    }
+
+}
